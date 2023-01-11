0e8ef4dbfba58d59cd12ae4cf71146bc8ad88e11
David Santiago Muñoz <david20041124@gmail.com>
2023-01-11 18:57:38 -0500

    FINAL VERSION

--- hola.java
+++
@@ -1,9 +0,0 @@
-public class hola {
-    
-    public static void main(String[] args) {
-        int [] arreglo = new int[10];
-
-        int valor = arreglo[0];
-        System.out.println( valor);
-    }
-}
